e877b318eac7e4159bdc96c14687841ea1168c4e
Amit Rokade <arokade@mach-3.opu.tv>
2020-08-10 15:40:24 -0400

    tests for graphs

--- graph_visualization/tests/test_tf_d3_graph.py
+++ graph_visualization/tests/test_tf_d3_graph.py
@@ -1,23 +1,36 @@
-import tf_proto_to_json
-from sdk2.common import py_test_util
+import os
+import shutil
+import tempfile
+import unittest
+
+from graph_visualization import tf_proto_to_json
+from graphs import sample_tf_graph
 
 
-class test_tf_d3_graph(py_test_util.PythonTestCase):
+class test_tf_d3_graph(unittest.TestCase):
+
+    def setUp(self):
+        self.tmp_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp_dir)
 
     def _import_sample_tf_graph(self):
-        pass
+        return sample_tf_graph.create_sample_tf_model(self.tmp_dir)
 
     def test_tf_d3_graph(self):
-        graph = ""
-        tf_proto_to_json.main(graph)
+        sample_graph_path = self._import_sample_tf_graph()
+        self.assertTrue(os.path.exists(sample_graph_path))
+        tf_proto_to_json.main(sample_graph_path)
 
     def test_tf_graph_with_ctrl_inputs(self):
-        graph = ""
-        tf_proto_to_json(graph)
+        graph_path = os.path.join("graphs", "tensorflow_inception_graph.pb")
+        self.assertTrue(os.path.exists(graph_path))
+        tf_proto_to_json.main(graph_path)
 
     def test_folder_path(self, path):
-        pass
+        self.assertEqual(os.path.exists(path), True)
 
 
 if __name__ == "__main__":
-    py_test_util.main()
+    unittest.main()
